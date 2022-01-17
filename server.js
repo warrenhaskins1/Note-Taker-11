0e1f279e3207bc6420bb591b17f0986d3873e5fa
Warren Haskins <warrenhaskins1@gmail.com>
2022-01-17 16:13:51 -0500

    test

--- server.js
+++ server.js
@@ -2,11 +2,15 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
+
+//Set the PORT so that it doesnt auto default to 3001 or you may get errors
 const PORT = process.env.Port || 3001;
 
 app.use(express.static('public'));
 
 //Add our middleware for json and urlencoded
+app.use(express.json());
+app.use(express.urlencoded({ extended : true}));
 
 //Add folder for routes to seperate concerns
 
