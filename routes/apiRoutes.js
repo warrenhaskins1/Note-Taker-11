7e8c04d53989c833a837a0ce0f043f91fbc2a51a
Warren Haskins <warrenhaskins1@gmail.com>
2022-01-19 19:30:05 -0500

    test

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,43 +1,10 @@
-// const router = require('express').Router();
-// const store = require('../db/savedNotes');
-
-// // requesting the existing notes
-
-// router.get('/notes', (req, res) => {
-//     console.log(req.body)
-//     store
-//         .getNotes()
-//         .then(notes => {
-//             res.json(notes)
-//         })
-//         .catch(err => {
-//             res.status(500).json(err)
-//         })
-// })
-
-// // posting note function route 
-
-// router.post('/notes', (req, res) => {
-//     console.log(req.body)
-//     store
-//         .addNote(req.body)
-//         .then(note => {
-//             res.json(note)
-//         })
-//         .catch(err => {
-//             res.status(500).json(err)
-//         })
-// })
-
-
-// module.exports = router;
 const router = require('express').Router();
-
-const store = require('../db/store');
+const store = require('../db/savedNotes');
 
 // requesting the existing notes
 
 router.get('/notes', (req, res) => {
+    console.log(req.body)
     store
         .getNotes()
         .then(notes => {
@@ -62,14 +29,5 @@ router.post('/notes', (req, res) => {
         })
 })
 
-
-// delete note function route
-
-router.delete('/notes/:id', (req, res) => {
-    store
-        .removeNote(req.params.id)
-        .then(() => res.json({ ok: true }))
-        .catch(err => res.status(500).json(err))
-})
 
 module.exports = router;
