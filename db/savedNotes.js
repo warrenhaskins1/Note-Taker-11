ee5e0cd8b84dac91861180c7d763b25531574e6f
Warren Haskins <warrenhaskins1@gmail.com>
2022-01-19 19:16:12 -0500

    test

--- db/savedNotes.js
+++ db/savedNotes.js
@@ -1,6 +1,6 @@
 const util = require("util");
 const fs = require("fs");
-const uuid = require("uuid").v1;
+const uuid = require("uuid").v4;
 
 const readFileAsync = util.promisify(fs.readFile);
 const writeFileAsync = util.promisify(fs.writeFile);
