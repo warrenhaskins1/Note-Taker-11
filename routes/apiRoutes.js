8520634141404ee9787a41c42b27a491a9a39b6f
Warren Haskins <warrenhaskins1@gmail.com>
2022-01-19 19:27:56 -0500

    test

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,5 +1,39 @@
+// const router = require('express').Router();
+// const store = require('../db/savedNotes');
+
+// // requesting the existing notes
+
+// router.get('/notes', (req, res) => {
+//     console.log(req.body)
+//     store
+//         .getNotes()
+//         .then(notes => {
+//             res.json(notes)
+//         })
+//         .catch(err => {
+//             res.status(500).json(err)
+//         })
+// })
+
+// // posting note function route 
+
+// router.post('/notes', (req, res) => {
+//     console.log(req.body)
+//     store
+//         .addNote(req.body)
+//         .then(note => {
+//             res.json(note)
+//         })
+//         .catch(err => {
+//             res.status(500).json(err)
+//         })
+// })
+
+
+// module.exports = router;
 const router = require('express').Router();
-const store = require('../db/savedNotes');
+
+const store = require('../db/store');
 
 // requesting the existing notes
 
@@ -28,5 +62,14 @@ router.post('/notes', (req, res) => {
         })
 })
 
+
+// delete note function route
+
+router.delete('/notes/:id', (req, res) => {
+    store
+        .removeNote(req.params.id)
+        .then(() => res.json({ ok: true }))
+        .catch(err => res.status(500).json(err))
+})
 
 module.exports = router;
